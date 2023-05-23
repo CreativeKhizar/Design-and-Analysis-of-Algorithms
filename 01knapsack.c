8de9f73b6a7b50201c06331e7bf5908ff802168b
CreativeKhizar <119342245+CreativeKhizar@users.noreply.github.com>
2023-05-23 09:37:58 +0530

    adding algorithm

--- 01knapsack.c
+++ 01knapsack.c
@@ -47,3 +47,21 @@ int main()
 	printf("The maximum profit is %d",profit);
 	return 0;
 }
+
+
+/*
+
+Algorithm for the above code
+
+Start by declaring and initializing necessary variables and arrays.
+Read the maximum weight (mw) and the total number of items (n) from the user.
+Read the weights of the items (w[i]) using a loop that runs from 0 to n-1.
+Read the profits of the items (p[i]) using a similar loop.
+Calculate the value density (vd[i]) for each item by dividing its profit by its weight.
+Sort the items in descending order based on their value density, using a bubble sort algorithm.
+Iterate through the sorted items and add them to the knapsack as long as the total weight (tw) does not exceed the maximum weight (mw).
+If adding the current item would exceed mw, continue to the next item.
+Otherwise, add the item's weight to tw and its profit to the total benefit (benefit).
+Print the calculated benefit.
+End the program.
+*/
